051288b7e90e7e87e67ba47abf7399a66fee2627
cp6g18 <cp6g18@soton.ac.uk>
2021-08-23 17:17:13 +0100

    Commit before refactoring

--- src/main/java/View/Editor/EditorFile.java
+++ src/main/java/View/Editor/EditorFile.java
@@ -5,10 +5,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.OutputStream;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import javafx.geometry.Insets;
